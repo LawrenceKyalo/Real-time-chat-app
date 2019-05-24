925ca8fa8f6a920276b566210bf916680b94cf35
Lawrence <lawrencekkyalo@gmail.com>
2019-05-24 15:14:57 +0300

    Update main.js

--- main.js
+++ main.js
@@ -16,5 +16,6 @@ const form = document.querySelector("form");
 
     function addMessage(message) {
         const li = document.createElement("li");
+        li.innerHTML = message;
         
     }
