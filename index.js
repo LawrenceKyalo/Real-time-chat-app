4fb101c1b717c658785048dbcefe8882bf90293d
Lawrence <lawrencekkyalo@gmail.com>
2019-05-24 15:35:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,9 @@ io.on("connection", function(socket) {
         });
 
         socket.on("chat_message", function(data) {
-            
+            data.username = this.username;
+            socket.broadcast.emit("chat_message", data);
+        });
 
         socket.on("disconnect", function(data) {
             socket.broadcast.emit("user_leave", this.username);
