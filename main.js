79fa79e3e4ccd5b144f845898ddcd0b8a06ec275
Lawrence <lawrencekkyalo@gmail.com>
2019-05-24 15:12:46 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const form = document.querySelector("form");
     const input = document.querySelector(".input");
     const messages = document.querySelector(".messages");
+    const username = prompt("Please enter a username: ", "");
+
     
