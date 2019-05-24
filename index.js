cbf906a7c871f045827f9661c50a15b00af012d0
Lawrence <lawrencekkyalo@gmail.com>
2019-05-24 15:45:37 +0300

    updated index.html

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-	const io = require("socket.io")(http);
 	const app = require("express")();
     const http = require("http").Server(app);
+    const io = require("socket.io")(http);
     const port = process.env.PORT || 3000;
 
     app.get("/", function(req, res) {
         res.sendFile(__dirname + "/index.html");
     });
 
-    http.listen(port, function() {
-        console.log("Listening on *:" + port);
-    });
-
-io.on("connection", function(socket) {
+    io.on("connection", function(socket) {
 
         socket.on("user_join", function(data) {
             this.username = data;
@@ -26,4 +22,8 @@ io.on("connection", function(socket) {
         socket.on("disconnect", function(data) {
             socket.broadcast.emit("user_leave", this.username);
     	});
+    });
+
+    http.listen(port, function() {
+        console.log("Listening on *:" + port);
     });
